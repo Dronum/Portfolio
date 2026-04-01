74da34c442d198f87ddd45f4843ff795bdd7c04e
Vaish <v.logendra1993@gmail.com>
2026-04-02 00:37:06 +0200

    heroname added

--- portfolio/src/app/app.ts
+++ portfolio/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from "./shared/header/header";
+import { Header } from './shared/header/header';
+import { Hero } from './content/hero/hero';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Hero],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('portfolio');
