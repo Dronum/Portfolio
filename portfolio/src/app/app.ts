2d7444c4a9b6801037ce11bf8cde74fbd6d9f861
Vaish <v.logendra1993@gmail.com>
2026-04-06 19:12:01 +0200

    footer added

--- portfolio/src/app/app.ts
+++ portfolio/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/header/header';
 import { Hero } from './content/hero/hero';
+import { Footer } from "./shared/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Hero],
+  imports: [RouterOutlet, Header, Hero, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
